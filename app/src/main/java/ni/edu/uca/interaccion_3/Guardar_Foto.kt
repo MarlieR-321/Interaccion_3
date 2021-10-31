7d6cff8520ec9f72afa7c289b9fb2bd5629c2303
MarlieR-321 <marlie.ramirez12169@est.uca.edu.ni>
2021-10-30 21:20:29 -0600

    Guardar en Galeria

--- app/src/main/java/ni/edu/uca/interaccion_3/Guardar_Foto.kt
+++ app/src/main/java/ni/edu/uca/interaccion_3/Guardar_Foto.kt
@@ -11,10 +11,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
-import android.util.Log
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
-import androidx.core.content.FileProvider
 import ni.edu.uca.interaccion_3.databinding.ActivityGuardarFotoBinding
 import java.io.*
 
@@ -23,8 +21,6 @@ class Guardar_Foto : AppCompatActivity() {
     private val REQUEST_PERMISSION_CAMERA =100
     private val REQUEST_IMAGE_CAMERA =101
     private lateinit var imgBitmap:Bitmap
-    private var ruta = ""
-    val cameraintent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding= ActivityGuardarFotoBinding.inflate(layoutInflater)
@@ -74,57 +70,87 @@ class Guardar_Foto : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode,resultCode,data)
         if (requestCode == REQUEST_IMAGE_CAMERA && resultCode == RESULT_OK) {
-            //imgBitmap = data?.extras?.get("data") as Bitmap
-                // Uri.parse(ruta)
-            binding.ivImagen.setImageURI(Uri.parse(ruta))
+            imgBitmap = data?.extras?.get("data") as Bitmap
+            binding.ivImagen.setImageBitmap(imgBitmap)
         }
     }
 
     //tomar y mostrar la foto
     private fun goToCamera() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAMERA)
+            }
+        }
+    }
 
-        if(cameraintent.resolveActivity(packageManager)!=null){
+    private fun savePic(): Uri? {
+        var fos: OutputStream? = null
+        var file: File? = null
+        var uri: Uri? = null
 
-            /*var imagenArchivo:File? = null
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
 
-            try {
-                imagenArchivo = crearImagen()
-            }catch (e:IOException){
-                Log.e("Error: ",e.toString())
+            val resolver = this.contentResolver
+            val fileName = ("${binding.etNombreFoto.text.toString()}")
+            val values = ContentValues().apply {
+                put(MediaStore.Images.Media.DISPLAY_NAME, fileName)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/MyApp")
+                put(MediaStore.Images.Media.IS_PENDING, 1)
             }
 
-            if(imagenArchivo!=null){
-                val foto: Uri = FileProvider.getUriForFile(this,"ni.edu.uca.interaccion_3",imagenArchivo)
-                cameraintent.putExtra(MediaStore.EXTRA_OUTPUT,foto)
-            }*/
+            uri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
 
-            startActivityForResult(cameraintent, REQUEST_IMAGE_CAMERA)
+            try {
+                fos = uri?.let { resolver.openOutputStream(it) }
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
 
+            values.clear()
+            values.put(MediaStore.Images.Media.IS_PENDING, 0)
+
+            if (uri != null) {
+                resolver.update(uri, values, null, null)
+            }
+
+        } else {
+            val imageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES).toString()
+            val fileName = ("${System.currentTimeMillis()}${".jpg"}")
+
+            file = File(imageDir, fileName)
+            try {
+                fos = FileOutputStream(file)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            }
         }
-    }
-
-    //guardar la imagen con nombre
-    private fun savePic() {
-        var imagenArchivo:File? = null
-
-        try {
-            imagenArchivo = crearImagen()
-        }catch (e:IOException){
-            Log.e("Error: ",e.toString())
+        val save: Boolean = imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos)
+        if (save) {
+            Toast.makeText(this, "Picture save successfully", Toast.LENGTH_SHORT).show()
         }
 
-        if(imagenArchivo!=null){
-            val foto: Uri = FileProvider.getUriForFile(this,"ni.edu.uca.interaccion_3",imagenArchivo)
-            cameraintent.putExtra(MediaStore.EXTRA_OUTPUT,foto)
+        fos?.run {
+            flush()
+            close()
         }
-    }
 
-    private fun crearImagen():File{
-        val nmbImagen = binding.etNombreFoto.text.toString()
-        val descrImagen = binding.etDescripcion.text.toString()
-        val directorio: File? = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val imagen = File.createTempFile(nmbImagen,".jpg",directorio)
-        ruta = imagen.absolutePath
-        return imagen
+        if (fos != null) {
+            try {
+                fos.flush()
+                fos.close()
+
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+
+        if (file != null) {
+            //Metodo para buscar nuevos directorios en la galeria y colocar los datos de la img antes descritos
+            MediaScannerConnection.scanFile(this, arrayOf(file.toString()), null, null)
+        }
+
+        return uri
     }
 }
