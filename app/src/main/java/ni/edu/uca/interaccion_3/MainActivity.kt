39927630ba6815ee1074452bf27c36a6fe1f4798
Cath1999 <72618362+Cath1999@users.noreply.github.com>
2021-11-01 13:05:16 -0600

    Update MainActivity.kt

--- app/src/main/java/ni/edu/uca/interaccion_3/MainActivity.kt
+++ app/src/main/java/ni/edu/uca/interaccion_3/MainActivity.kt
@@ -6,7 +6,8 @@ import android.os.Bundle
 import ni.edu.uca.interaccion_3.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding:ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
+    val u = Usuario("Mar", "abc123", "1232423", "Marlie")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -14,7 +15,19 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnIniciar.setOnClickListener {
-            startActivity(Intent(this,Plano_Foto::class.java))
+            if (binding.tvnUsuario.text.isNullOrBlank() && binding.tvCont.text.isNullOrBlank()) {
+
+                Toast.makeText(this, "Por favor llena los campos", Toast.LENGTH_LONG).show()
+            } else {
+                if (binding.etNombreUsuario.text.toString() == u.Usuario.toString() && binding.etContrasena.text.toString() == u.contras.toString()) {
+                    startActivity(Intent(this, Plano_Foto::class.java))
+                    Toast.makeText(this, "Bienvenido: ${u.NombreReal}", Toast.LENGTH_LONG).show()
+
+                } else {
+                    Toast.makeText(this, "Usuarios y contraseña incorrectos", Toast.LENGTH_LONG)
+                        .show()
+                }
+            }
         }
     }
 }
